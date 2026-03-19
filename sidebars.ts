a883d78ad892dce20ab73c9db6165433d47dd28c
Pierre-Alban <pierre-alban.louradour@epitech.eu>
2026-03-19 11:01:59 +0100

    Added a section on shared dynamic libraries in the CPP documentation

--- sidebars.ts
+++ sidebars.ts
@@ -12,6 +12,7 @@ module.exports = {
                         'advanced-programming-languages/cpp/preambule',
                         'advanced-programming-languages/cpp/concepts-avances-poo',
                         'advanced-programming-languages/cpp/design-patterns',
+                        'advanced-programming-languages/cpp/shared-dynamic-libraries',
                     ]
                 },
             ]
